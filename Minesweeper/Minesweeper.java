a56c7ab932a57aa6259bd726c3e600e959bef2a0
Ilusion9 <40362576+Ilusion9@users.noreply.github.com>
2019-10-23 15:54:41 +0300

    Update Minesweeper.java

--- Minesweeper/Minesweeper.java
+++ Minesweeper/Minesweeper.java
@@ -1,4 +1,4 @@
-package Minesweeper;
+package Mineswepeer;
 
 import javafx.application.Application;
 import javafx.application.Platform;
@@ -6,18 +6,21 @@ import javafx.stage.Stage;
 
 public class Minesweeper extends Application
 {
+	/* Draw the board and make the rules when the application is launched */
 	public void start(Stage primaryStage)
 	{
 		new Board(primaryStage);
 	}
 
+	/* When the application is closed */
 	public void stop()
 	{
 		Platform.exit();
 	}
 	
+	/* Launch the application */
 	public static void main(String[] args)
 	{
-		Application.launch(args);
+		launch(args);
 	}
 }
